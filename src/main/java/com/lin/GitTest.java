ec3e776ba6624dd8be227040cb1f117b6f21443d
lin <lin@qq.com>
2021-09-06 21:11:29 +0800

    初次提交

---
+++ src/main/java/com/lin/GitTest.java
@@ -0,0 +1,11 @@
+package com.lin;
+
+/**
+ * @Author xiaoqiong
+ * @Date 2021/9/6
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}

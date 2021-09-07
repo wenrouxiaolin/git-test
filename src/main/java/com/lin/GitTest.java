dd24e37ea7df44d6ef07cb07aa4c5d7a75593b71
lin <lin@qq.com>
2021-09-07 09:09:19 +0800

    ssh-git-test

--- src/main/java/com/lin/GitTest.java
+++ src/main/java/com/lin/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git4!");
+        System.out.println("ssh-git push test!");
     }
 }
